f3c0f5e3d557e0880d598307d9143ad6b9dfc99f
Doğukan Topallar <75544671+dogukanqqq@users.noreply.github.com>
2021-06-01 01:11:20 +0300

    Add image functions
    image.size
    image.dtype
    image.shape etc.

--- openImage.py
+++ openImage.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
 
-image=cv2.imread("001.jpg",0) #0 gri yapar
+image=cv2.imread("001.jpg",0) # makes "0" grey
 
-cv2.imshow("Dogukan Topallar vesikalik",image)
+cv2.imshow("Dogukan Topallar vesikalik",image) #show image"001.jpg"
+
+print(image)        #how many pixels - with matrix
+print(image.size)   #size output
+print(image.dtype)  #data informations
+print(image.shape)  #width, height, how many channels are used
+                    #When the gray picture is made, the channel is not specified
+                    #in this way, memory and speed are advantageous
 
 
-cv2.waitKey(0)              #cıkıs fonksiyonları   
-cv2.destroyAllWindows()     #cıkıs fonksıyonları
+cv2.waitKey(0)              #function used to log out
+cv2.destroyAllWindows()     #function used to log out
